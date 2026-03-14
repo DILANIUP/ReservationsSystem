b8cc87df5b96a432737dad9f6f7774e008834f9e
migue1angel <macf06200@gmail.com>
2026-03-13 20:41:20 -0500

    refactor: remove old appsettings.json and add new configuration file

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using ReservationsSystem.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // EF Core + SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
